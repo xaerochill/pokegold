66cd19a56bda28f5141184d0a246b466e9a63175
xaerochill <22112853+xaerochill@users.noreply.github.com>
2026-06-11 21:55:36 +0200

    Diversify `ruins_of_alph` tileset

--- data/tilesets/ruins_of_alph_collision.asm
+++ data/tilesets/ruins_of_alph_collision.asm
@@ -19,8 +19,8 @@
 	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 12
 	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 13
 	tilecoll WALL, FLOOR, WALL, WALL ; 14
-	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 15
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 16
+	tilecoll WALL, WALL, WALL, FLOOR ; 15
+	tilecoll WALL, WALL, FLOOR, WALL ; 16
 	tilecoll FLOOR, WALL, WALL, WALL ; 17
 	tilecoll WALL, WALL, WALL, PIT ; 18
 	tilecoll WALL, WALL, PIT, WALL ; 19
@@ -31,15 +31,15 @@
 	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 1e
 	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 1f
 	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 20
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 21
-	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 22
-	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 23
-	tilecoll WALL, WALL, WALL, WALL ; 24
-	tilecoll WALL, WALL, WALL, WALL ; 25
+	tilecoll DOOR, WALL, FLOOR, FLOOR ; 21
+	tilecoll WARP_CARPET_DOWN, WARP_CARPET_DOWN, WALL, WALL ; 22
+	tilecoll 88, FLOOR, 88, FLOOR ; 23
+	tilecoll 88, FLOOR, 88, FLOOR ; 24
+	tilecoll 88, FLOOR, 88, FLOOR ; 25
 	tilecoll WALL, WALL, WALL, WALL ; 26
 	tilecoll WALL, WALL, WALL, WALL ; 27
 	tilecoll WALL, WALL, WALL, WALL ; 28
-	tilecoll WALL, WALL, WALL, WALL ; 29
+	tilecoll 88, FLOOR, 88, FLOOR ; 29
 	tilecoll WALL, WALL, WALL, WALL ; 2a
 	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, WALL ; 2b
 	tilecoll FLOOR, FLOOR, WALL, WARP_CARPET_DOWN ; 2c
