a4975a45495757c697e73a92ca8966d69fbd196e
xaerochill <22112853+xaerochill@users.noreply.github.com>
2026-06-11 21:59:38 +0200

    Diversify `port` tileset

--- data/tilesets/port_collision.asm
+++ data/tilesets/port_collision.asm
@@ -1,7 +1,7 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 00
 	tilecoll WALL, WALL, WALL, WALL ; 01
 	tilecoll BUOY, WATER, BUOY, WATER ; 02
-	tilecoll WATER, BUOY, WATER, BUOY ; 03
+	tilecoll BUOY, WATER, BUOY, BUOY ; 03
 	tilecoll LADDER, LADDER, LADDER, LADDER ; 04
 	tilecoll WALL, WALL, WALL, WATER ; 05
 	tilecoll WALL, WALL, WATER, WATER ; 06
@@ -14,13 +14,13 @@
 	tilecoll WALL, WATER, WALL, WALL ; 0d
 	tilecoll WATER, WATER, WALL, WALL ; 0e
 	tilecoll WATER, WALL, WALL, WALL ; 0f
-	tilecoll WATER, WATER, WATER, WATER ; 10
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 11
-	tilecoll WATER, WATER, WATER, WATER ; 12
+	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 10
+	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 11
+	tilecoll WALL, WALL, WALL, WALL ; 12
 	tilecoll WATER, FLOOR, WATER, FLOOR ; 13
 	tilecoll WATER, FLOOR, WATER, FLOOR ; 14
-	tilecoll WATER, WATER, WATER, WATER ; 15
-	tilecoll WATER, WATER, WATER, WATER ; 16
+	tilecoll WALL, WALL, BUOY, WATER ; 15
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 16
 	tilecoll WATER, FLOOR, WALL, WARP_CARPET_DOWN ; 17
 	tilecoll WATER, WATER, WALL, WALL ; 18
 	tilecoll WATER, FLOOR, WALL, WALL ; 19
@@ -31,34 +31,19 @@
 	tilecoll WATER_21, WATER_21, WALL, WALL ; 1e
 	tilecoll WALL, WALL, WALL, WATER ; 1f
 	tilecoll WALL, FLOOR, WATER, WARP_CARPET_DOWN ; 20
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 21
-	tilecoll BUOY, BUOY, BUOY, WATER ; 22
-	tilecoll BUOY, BUOY, WATER, BUOY ; 23
-	tilecoll BUOY, BUOY, WATER, WATER ; 24
-	tilecoll BUOY, BUOY, WATER, WATER ; 25
-	tilecoll LADDER, LADDER, LADDER, LADDER ; 26
-	tilecoll FLOOR, FLOOR, WATER, WATER ; 27
-	tilecoll FLOOR, FLOOR, FLOOR, WATER ; 28
-	tilecoll FLOOR, FLOOR, WATER, FLOOR ; 29
-	tilecoll WALL, WALL, WALL, WALL ; 2a
-	tilecoll WALL, WALL, FLOOR, FLOOR ; 2b
-	tilecoll FLOOR, WATER, FLOOR, FLOOR ; 2c
-	tilecoll WATER, FLOOR, FLOOR, FLOOR ; 2d
+	tilecoll WATER, WATER, BUOY, BUOY ; 21
+	tilecoll FLOOR, FLOOR, WATER, WATER ; 22
+	tilecoll FLOOR, FLOOR, WATER, FLOOR ; 23
+	tilecoll WATER, FLOOR, FLOOR, FLOOR ; 24
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 25
+	tilecoll FLOOR, WATER, FLOOR, WATER ; 26
+	tilecoll WATER, FLOOR, WATER, FLOOR ; 27
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 28
+	tilecoll WALL, FLOOR, WALL, FLOOR ; 29
+	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, WARP_CARPET_DOWN ; 2a
+	tilecoll FLOOR, WATER, FLOOR, WATER ; 2b
+	tilecoll FLOOR, FLOOR, FLOOR, LADDER ; 2c
+	tilecoll WATER, WATER, WATER, WATER ; 2d
 	tilecoll FLOOR, WATER, FLOOR, WATER ; 2e
 	tilecoll WATER, FLOOR, WATER, FLOOR ; 2f
-	tilecoll FLOOR, FLOOR, WATER, WATER ; 30
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 31
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 32
-	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, WARP_CARPET_DOWN ; 33
-	tilecoll FLOOR, WATER, FLOOR, WATER ; 34
-	tilecoll WALL, WALL, FLOOR, LADDER ; 35
-	tilecoll WATER, WATER, WATER, WATER ; 36
-	tilecoll FLOOR, WATER, FLOOR, WATER ; 37
-	tilecoll WATER, FLOOR, WATER, FLOOR ; 38
-	tilecoll WATER, WATER, FLOOR, FLOOR ; 39
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3a
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3b
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3c
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3d
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3e
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 3f
+	tilecoll WATER, WATER, FLOOR, FLOOR ; 30
